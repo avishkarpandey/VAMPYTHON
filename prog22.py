d40524bb477b0f9a8eccf8a722fc9277038551dd
avishkarpandey <avishkarpandey105346@gmail.com>
2024-10-10 12:59:36 +0530

    modification

--- prog22.py
+++ prog22.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 years = np.array([2020, 2021, 2022, 2023, 2024])
 sales = np.array([89, 92, 95, 88, 99])
 plt.title("Sale Growth of JIO")
-plt.plot(years, sales, marker = 'o')
+plt.bar(years, sales)
 plt.xlabel("Years")
-plt.ylabel("Sales")
+plt.ylabel("Sales in %")
+
 plt.show()
